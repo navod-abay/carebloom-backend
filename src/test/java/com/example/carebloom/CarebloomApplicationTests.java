3afa641b42475da139204b287b4212da1b391b13
emlnvd <emil.navod@gmail.com>
2025-06-15 11:19:02 +0530

    Initial COmmit

---
+++ src/test/java/com/example/carebloom/CarebloomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carebloom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarebloomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
